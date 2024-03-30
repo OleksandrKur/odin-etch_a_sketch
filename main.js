6851b8eebc4ace5366820faf540dcdd18ad2a06e
OleksandKur <136488587+OleksandrKur@users.noreply.github.com>
2024-03-30 16:08:44 +0200

    add change resolution button and reset button

--- main.js
+++ main.js
@@ -1,25 +1,53 @@
-const NUMBEROFSQARES = 16;
 const container = document.querySelector(".container");
+let resolutionButton = document.getElementById("set_resolution");
+
 fillCanvas();
+draw();
 
 
-let canvas = document.querySelectorAll(".pixel");
+function draw(){
+    let canvas = document.querySelectorAll(".pixel");
 canvas.forEach(pixel => {
-    pixel.addEventListener("click", event => {
-        event.target.style.backgroundColor = "black";
-    });
     pixel.addEventListener("mouseover", event => {
-        event.target.style.backgroundColor = "black";
+        event.target.style.backgroundColor = "#292521";
     });
 })
+}
+
+function removeCanvas(){
+    let canvas = document.querySelectorAll(".pixel");
+    canvas.forEach(pixel => {
+        pixel.remove();
+    });
+}
+
+function erase(){
+    let canvas = document.querySelectorAll(".pixel");
+    canvas.forEach(pixel => {
+        pixel.style.backgroundColor = "unset";
+        console.log(pixel.style.backgroundColor);
+    });
+}
 
 
-
-
-function fillCanvas(){
-    for (let i = 0; i < NUMBEROFSQARES*NUMBEROFSQARES; i++){
+function fillCanvas(squares = 16){
+    for (let i = 0; i < squares*squares; i++){
         const pixel = document.createElement("div");
+        pixel.style.width = `${100 / squares}%`
         pixel.classList.add("pixel");
         container.appendChild(pixel);
     }
+}
+
+function changeResolution(){
+    let resolution = parseInt(prompt("Please enter a number (maximum 100)."));
+    if (resolution > 0 && resolution <= 100){
+        removeCanvas();
+        fillCanvas(resolution);
+        draw();
+    }
+    else{
+        alert("Valid input: number (1-100)")
+    }
+
 }
